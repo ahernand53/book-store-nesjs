dd6d5d12475828a4e5d728c4b0a458a4fe327ce4
Asdrubal Hernandez <ahernand.development@gmail.com>
2021-02-04 09:10:48 -0500

    add guards with roles

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -12,12 +12,16 @@ import {
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../role/decorators/role.decorator';
+import { RoleGuard } from '../role/guards/role.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly _userService: UserService) {}
 
   @Get(':id')
+  @Roles('ADMIN', 'AUTHOR')
+  @UseGuards(AuthGuard, RoleGuard)
   async getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
     return await this._userService.get(id);
   }
